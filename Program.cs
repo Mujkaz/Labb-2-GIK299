434bf6e3b78dd464d87d91b8c8d1566e7f8e5aa4
Mujkaz <h23vilmu@du.se>
2023-11-26 20:00:19 +0100

    New updated version to fit assignment 2.

--- Program.cs
+++ Program.cs
@@ -1,86 +1,161 @@
 ﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
 
 namespace GIK299_Laboration_2_h23vilmu
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            //Array
+
+            //Samling av variablar.
+            int[] familyMemberAges;
+            string[] familyMemberNames;
+            int totalAge = 0;
 
 
+            //Skapande av arrayer före meny för att ha globala varianblar
+            Console.WriteLine("Skriv antal familjemedlemmar du vill lägga till.");
+            int numberOfMember = Int32.Parse(Console.ReadLine());
+            familyMemberAges = new int[numberOfMember];
+            familyMemberNames = new string[numberOfMember];
+            Console.WriteLine("Du har skrivit in " + numberOfMember + " st familjemedlemmar, var god välj det första alternativet i menyn.");
 
-            Console.WriteLine("Hur många familjemedlemmar har du?");
-            int numberOfMember = int.Parse(Console.ReadLine());
-
-            string[] familyMember = new string[numberOfMember];
-
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
 
 
-
-            for (int i = 0; i < numberOfMember; i++)
-
+            //Meny
+            int menuSelect = 0;
+            while (menuSelect != 5)
             {
-                Console.WriteLine("Vad heter" + familyMember[i]);
-                Console.ReadLine()
+
+                Console.WriteLine("Välj följande alternativ." +
+            "\n [1] Skriv familjemedlemmars ålder och namn." +
+            "\n [2] Visa alla tillagda familjemedlemmar." +
+            "\n [3] Visa totalålder" +
+            "\n [4] Visa medelålder" +
+            "\n [5] Avsluta programmet");
+                menuSelect = Int32.Parse(Console.ReadLine());
+
+
+                //Switch sats för att meny ska få funktioner.
+                switch (menuSelect)
+                {
+                    case 1:
+
+                        //Här fyller jag arrayerna med data som användaren skriver.
+                        Console.Clear();
+                        Console.WriteLine("Skriv familjemedlemmarnas namn och ålder.");
+
+                        for (int i = 0; i < numberOfMember; i++)
+                        {
+                            Console.WriteLine($"Skriv medlem: {i + 1} namn:");
+                            familyMemberNames[i] = Console.ReadLine();
+
+                            Console.WriteLine($"Skriv medlem: {i + 1} ålder:");
+                            familyMemberAges[i] = int.Parse(Console.ReadLine());
+                        }
+
+
+                        //beräknar totalålder om den är noll till andra case för att få fram totalålder.
+                        if (totalAge == 0)
+                        {
+                            for (int i = 0; i < numberOfMember; i++)
+                            {
+                                totalAge += familyMemberAges[i];
+                            }
+                        }
+
+                        Console.Clear();
+                        Console.WriteLine("Alla namn och ålder har nu sparats. Var god återgå till meny och välj det andra alternativet.");
+
+                        break;
+
+                    case 2:
+
+                        Console.Clear();
+                        Console.WriteLine("Sammanställning:");
+                        Console.WriteLine("");
+
+                        //for loop för att kunna sammanställa familjemedlemmarna. Tar från arrayerna som jag skapat sedan tidigare.
+                        for (int i = 0; i < numberOfMember; i++)
+                        {
+
+                            string compilationMembers = "Namn: " + familyMemberNames[i] + "\t" + "Ålder: " + familyMemberAges[i];
+
+                            Console.WriteLine(compilationMembers);
+
+                        }
+
+                        Console.WriteLine("");
+
+                        Console.WriteLine("Var god återgå till meny och välj det tredje alternativet.");
+                        Console.WriteLine("");
+
+                        break;
+
+                    case 3:
+
+                        //Här tar jag fram data från if satsen jag gjorde i case 1 för att visa totalålder.
+                        Console.Clear();
+                        Console.WriteLine("Totalålder:");
+                        Console.WriteLine("");
+
+                        Console.WriteLine();
+
+                        Console.WriteLine("Totala åldern är: " + totalAge + " år");
+                        Console.WriteLine("");
+
+                        break;
+
+                    case 4:
+
+                        //Här har jag också tagit fram totalåldern från if-satsen jag gjorde i case 1 för att kunna presentera medelålder.
+                        Console.Clear();
+
+                        double averageAge = totalAge / numberOfMember;
+                        Console.WriteLine("Medelåldern är: " + averageAge.ToString("##.00") + " år.");
+
+                        Console.WriteLine("");
+                        break;
+
+                    case 5:
+                        Console.Clear();
+                        Console.WriteLine("Tack för visat intresse. :)");
+                        break;
+
+                    default:
+
+                        break;
+                }
+
             }
-
-            //Familjemedlem 1
-            Console.WriteLine("Skriv namnet på familjemedlem 1.");
-            string PersonOne = Console.ReadLine();
-            Console.WriteLine("Skriv även ålder.");
-            string PersonOneAgestr = Console.ReadLine();
-            int PersonOneAge = Convert.ToInt32(PersonOneAgestr);
-            Console.WriteLine("");
-
-
-            //Familjemedlem 2
-            Console.WriteLine("Skriv namnet på familjemedlem 2.");
-            string PersonTwo = Console.ReadLine();
-            Console.WriteLine("Skriv även ålder.");
-            string PersonTwoAgestr = Console.ReadLine();
-            int PersonTwoAge = Convert.ToInt32(PersonTwoAgestr);
-            Console.WriteLine("");
-
-
-            //Familjemedlem 3
-            Console.WriteLine("Skriv namnet på familjemedlem 3.");
-            string PersonThree = Console.ReadLine();
-            Console.WriteLine("Skriv även ålder.");
-            string PersonThreeAgestr = Console.ReadLine();
-            int PersonThreeAge = Convert.ToInt32(PersonThreeAgestr);
-            Console.WriteLine("");
-
-
-            //Familjemedlem 3
-            Console.WriteLine("Skriv namnet på familjemedlem 4.");
-            string PersonFour = Console.ReadLine();
-            Console.WriteLine("Skriv även ålder.");
-            string PersonFourAgestr = Console.ReadLine();
-            int PersonFourAge = Convert.ToInt32(PersonFourAgestr);
-            Console.WriteLine("");
-
-
-            //Sammanställning av åldrarna
-            Console.WriteLine(PersonOne + " är " + PersonOneAge + " år gammal.");
-            Console.WriteLine(PersonTwo + " är " + PersonTwoAge + " år gammal.");
-            Console.WriteLine(PersonThree + " är " + PersonThreeAge + " år gammal.");
-            Console.WriteLine(PersonFour + " är " + PersonFourAge + " år gammal.");
-            Console.WriteLine("");
-
-            //Total åldern för alla medlemmar
-            int TotalAge = PersonOneAge + PersonTwoAge + PersonThreeAge + PersonFourAge;
-            Console.WriteLine("Sammanlagda åldern är:" + TotalAge + " år.");
-
-            //Median ålder för alla medlemmar
-            double AvrageAge = TotalAge / 4.0; //
-            Console.WriteLine("Median ålder är: " + AvrageAge + "år.");
-
-
-
-
-
-            Console.ReadKey();
         }
+
+
     }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
